2efd37f41d4f29d2ad9f7eaadead43ea171b1474
kennethmarshall <kenneth02@gmail.com>
2018-08-19 20:24:40 +0100

    Adding exception stuff and trying Hamcrest

--- src/test/java/com/jakm/entities/BlockTest.java
+++ src/test/java/com/jakm/entities/BlockTest.java
@@ -4,7 +4,10 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 class BlockTest {
@@ -24,7 +27,17 @@ class BlockTest {
         Block classToTest = new Block();
 
         classToTest.setName("A");
-        assertEquals(classToTest.getName(), "A");
+
+        assertThat(classToTest.getName(), is(equalTo("A")));
+
+    }
+
+    @Test
+    void testNullThrowsException() {
+
+        Block classToTest = new Block();
+        assertThrows(Exception.class, () -> classToTest.getName());
+
     }
 
 }
