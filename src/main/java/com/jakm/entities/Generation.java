a041f148cbe3d2d281ba8c42725efcec8fe2557c
kennethmarshall <kenneth02@gmail.com>
2021-11-22 21:13:05 +0000

    Adding steps to the runGeneration

--- src/main/java/com/jakm/entities/Generation.java
+++ src/main/java/com/jakm/entities/Generation.java
@@ -1,12 +1,15 @@
 package com.jakm.entities;
 
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.IntStream;
 
 @Getter
+@Setter
 public class Generation {
 
     List<Plan> plans;
@@ -16,6 +19,11 @@ public class Generation {
     int maxLengthOfEachPlan;
 
     public Generation(List<Plan> plans, List<String> targetState, List<String> initialState, int howManyPlans, int maxLengthOfEachPlan) {
+
+        if (targetState == null || initialState == null || howManyPlans == 0 || maxLengthOfEachPlan == 0) {
+            throw new RuntimeException("Your problem configuration is incorrect. Program cannot continue");
+        }
+
         this.plans = plans;
         this.targetState = targetState;
         this.initialState = initialState;
@@ -25,9 +33,6 @@ public class Generation {
         //this is the first generation, so we need to create an object for the Plans
         if (this.plans == null) this.plans = new ArrayList<Plan>();
 
-        if (targetState == null || initialState == null || howManyPlans == 0 || maxLengthOfEachPlan == 0) {
-            throw new RuntimeException("Your problem configuration is incorrect. Program cannot continue");
-        }
     }
 
     public void createPlans() {
@@ -39,8 +44,78 @@ public class Generation {
         }
     }
 
-    public void runGeneration() {
+    public void runGeneration(int percentageOfBest, int percentageOfWorst, int numberOfMutations, int percentageToMutate) {
 
+        List<Plan> selectedPlans = new ArrayList<Plan>();
+
+        //sort the plans first.
+        this.plans.sort(Comparator.comparing(Plan::getPlanScore));
+
+        List<Plan> bestPlans = getBestPlans(percentageOfBest);
+
+        //select specified percentage of worst plans
+        List<Plan> worstPlans = getWorstPlans(percentageOfWorst);
+
+        List<Plan> parents = new ArrayList<>();
+        parents.addAll(bestPlans);
+        parents.addAll(worstPlans);
+
+        //mate the selected plans
+        List<Plan> nextGeneration = matePlans(parents);
+
+        //perform mutations on a percentage of the children
+
+    }
+
+    List<Plan> matePlans(List<Plan> parents) {
+        List<Plan> matedPlans = new ArrayList<>();
+
+        return matedPlans;
+    }
+
+
+    List<Plan> getBestPlans(int percentageOfBest) {
+
+        if (this.plans == null)
+            throw new UnsupportedOperationException("There are no plans set on this object, thus I cannot return the best");
+
+        if (this.plans.size() == 0)
+            throw new UnsupportedOperationException("There are no plans set on this object, thus I cannot return the best");
+
+        if (percentageOfBest == 0) return new ArrayList<Plan>();
+
+        List<Plan> bestPlans = new ArrayList<Plan>();
+        //select specified percentage of best plans
+        float numberOfPlansToPull = (float) percentageOfBest / 100 * this.plans.size();
+
+        //We are assuming at this point that the plans are already sorted so the best plans are first in the list
+        for (int i = 0; i < numberOfPlansToPull; i++) {
+            bestPlans.add(this.plans.get(i));
+        }
+
+        return bestPlans;
+    }
+
+    List<Plan> getWorstPlans(int percentageOfWorst) {
+
+        if (this.plans == null)
+            throw new UnsupportedOperationException("There are no plans set on this object, thus I cannot return the best");
+
+        if (this.plans.size() == 0)
+            throw new UnsupportedOperationException("There are no plans set on this object, thus I cannot return the best");
+
+        if (percentageOfWorst == 0) return new ArrayList<Plan>();
+
+        List<Plan> worstPlans = new ArrayList<Plan>();
+        //select specified percentage of best plans
+        float numberOfPlansToPull = (float) percentageOfWorst / 100 * this.plans.size();
+
+        //We are assuming at this point that the plans are already sorted so the best plans are first in the list
+        for (int i = this.plans.size() - 1; i >= (this.plans.size() - numberOfPlansToPull); i--) {
+            worstPlans.add(this.plans.get(i));
+        }
+
+        return worstPlans;
     }
 
 }
--- src/test/java/com/jakm/entities/GenerationTest.java
+++ src/test/java/com/jakm/entities/GenerationTest.java
@@ -1,5 +1,6 @@
 package com.jakm.entities;
 
+import com.jakm.interfaces.StackNames;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -19,9 +20,11 @@ public class GenerationTest {
     @BeforeEach
     void setUp() {
 
+
         plans = new ArrayList<Plan>();
         initialState = Arrays.asList("A", "B", "C");
         targetState = Arrays.asList("C", "B", "A");
+
 
     }
 
@@ -68,6 +71,159 @@ public class GenerationTest {
 
         actualMessage = exception.getMessage();
         assertTrue(actualMessage.contains(expectedMessageNumberDifferent));
+
+    }
+
+    @Test
+    void doesGenerationSelectRightNumberOfGoodPlans() {
+
+        List<String> initialState = Arrays.asList("B", "A", "C");
+        List<String> targetState = Arrays.asList("A", "B", "C");
+
+        Plan firstPlan = new Plan(3, initialState, targetState);
+        Plan secondPlan = new Plan(3, initialState, targetState);
+        Plan thirdPlan = new Plan(3, initialState, targetState);
+        Plan fourthPlan = new Plan(3, initialState, targetState);
+
+        firstPlan.setPlanScore(10);
+        secondPlan.setPlanScore(9);
+        thirdPlan.setPlanScore(8);
+        fourthPlan.setPlanScore(7);
+
+        classToTest = new Generation(plans, initialState, targetState, 100, 10);
+
+        classToTest.getPlans().add(firstPlan);
+        classToTest.getPlans().add(secondPlan);
+        classToTest.getPlans().add(thirdPlan);
+        classToTest.getPlans().add(fourthPlan);
+
+        List<Plan> bestPlans = classToTest.getBestPlans(50);
+
+        assertEquals(2, bestPlans.size());
+        assertTrue(bestPlans.contains(firstPlan));
+        assertTrue(bestPlans.contains(secondPlan));
+
+    }
+
+    @Test
+    void doesGenerationSelectRightNumberOfWorstPlans() {
+
+        List<String> initialState = Arrays.asList("B", "A", "C");
+        List<String> targetState = Arrays.asList("A", "B", "C");
+
+        Plan firstPlan = new Plan(3, initialState, targetState);
+        Plan secondPlan = new Plan(3, initialState, targetState);
+        Plan thirdPlan = new Plan(3, initialState, targetState);
+        Plan fourthPlan = new Plan(3, initialState, targetState);
+
+        firstPlan.setPlanScore(10);
+        secondPlan.setPlanScore(9);
+        thirdPlan.setPlanScore(8);
+        fourthPlan.setPlanScore(7);
+
+        classToTest = new Generation(plans, initialState, targetState, 100, 10);
+
+        classToTest.getPlans().add(firstPlan);
+        classToTest.getPlans().add(secondPlan);
+        classToTest.getPlans().add(thirdPlan);
+        classToTest.getPlans().add(fourthPlan);
+
+        List<Plan> worstPlans = classToTest.getWorstPlans(50);
+
+        assertEquals(2, worstPlans.size());
+        assertTrue(worstPlans.contains(thirdPlan));
+        assertTrue(worstPlans.contains(fourthPlan));
+
+    }
+
+    @Test
+    void doesGenerationSelectRightNumberOfWorstPlansWithTiedScores() {
+
+        List<String> initialState = Arrays.asList("B", "A", "C");
+        List<String> targetState = Arrays.asList("A", "B", "C");
+
+        Plan firstPlan = new Plan(3, initialState, targetState);
+        Plan secondPlan = new Plan(3, initialState, targetState);
+        Plan thirdPlan = new Plan(3, initialState, targetState);
+        Plan fourthPlan = new Plan(3, initialState, targetState);
+        Plan fifthPlan = new Plan(3, initialState, targetState);
+        Plan sixthPlan = new Plan(3, initialState, targetState);
+        Plan seventhPlan = new Plan(3, initialState, targetState);
+        Plan eithhPlan = new Plan(3, initialState, targetState);
+        Plan ninthPlan = new Plan(3, initialState, targetState);
+        Plan tenthPlan = new Plan(3, initialState, targetState);
+
+        firstPlan.setPlanScore(10);
+        secondPlan.setPlanScore(9);
+        thirdPlan.setPlanScore(8);
+        fourthPlan.setPlanScore(5);
+        fifthPlan.setPlanScore(5);
+        sixthPlan.setPlanScore(4);
+        seventhPlan.setPlanScore(4);
+        eithhPlan.setPlanScore(4);
+        ninthPlan.setPlanScore(4);
+        tenthPlan.setPlanScore(4);
+
+        classToTest = new Generation(plans, initialState, targetState, 100, 10);
+
+        classToTest.getPlans().add(firstPlan);
+        classToTest.getPlans().add(secondPlan);
+        classToTest.getPlans().add(thirdPlan);
+        classToTest.getPlans().add(fourthPlan);
+        classToTest.getPlans().add(fifthPlan);
+        classToTest.getPlans().add(sixthPlan);
+        classToTest.getPlans().add(seventhPlan);
+        classToTest.getPlans().add(eithhPlan);
+        classToTest.getPlans().add(ninthPlan);
+        classToTest.getPlans().add(tenthPlan);
+
+        List<Plan> worstPlans = classToTest.getWorstPlans(20);
+
+        assertEquals(2, worstPlans.size());
+        assertTrue(worstPlans.contains(tenthPlan));
+        assertTrue(worstPlans.contains(ninthPlan));
+
+    }
+
+    @Test
+    void mateTwoSimplePlans() {
+
+        List<String> initialState = Arrays.asList("B", "A", "C");
+        List<String> targetState = Arrays.asList("A", "B", "C");
+
+        classToTest = new Generation(plans, initialState, targetState, 2, 4);
+
+        Plan firstPlan = new Plan(4, initialState, targetState);
+        Step step1 = new Step(StackNames.ORIGINSTACK, StackNames.FIRSTSTACK);
+        Step step2 = new Step(StackNames.ORIGINSTACK, StackNames.FIRSTSTACK);
+        Step step3 = new Step(StackNames.ORIGINSTACK, StackNames.FIRSTSTACK);
+        Step step4 = new Step(StackNames.ORIGINSTACK, StackNames.FIRSTSTACK);
+        List<Step> steps1 = new ArrayList<>();
+        steps1.add(step1);
+        steps1.add(step2);
+        steps1.add(step3);
+        steps1.add(step4);
+        firstPlan.setSteps(steps1);
+
+        Plan secondPlan = new Plan(4, initialState, targetState);
+        Step step5 = new Step(StackNames.SECONDSTACK, StackNames.FIRSTSTACK);
+        Step step6 = new Step(StackNames.SECONDSTACK, StackNames.FIRSTSTACK);
+        Step step7 = new Step(StackNames.SECONDSTACK, StackNames.FIRSTSTACK);
+        Step step8 = new Step(StackNames.SECONDSTACK, StackNames.FIRSTSTACK);
+        List<Step> steps2 = new ArrayList<>();
+        steps2.add(step5);
+        steps2.add(step6);
+        steps2.add(step7);
+        steps2.add(step8);
+        secondPlan.setSteps(steps2);
+
+        List<Plan> parents = new ArrayList();
+        parents.add(firstPlan);
+        parents.add(secondPlan);
+
+        List<Plan> matedPlans = classToTest.matePlans(parents);
+
+        //assertEquals(matedPlans.size(), 2);
 
     }
 
